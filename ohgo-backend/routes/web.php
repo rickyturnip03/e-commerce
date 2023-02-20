1ac11814b8c6a6045bc372ed79fb0e7df8cc3add
rickyturnip03 <rickysuparmansaragih@student.uns.ac.id>
2023-02-20 16:14:19 +0700

    first commit

---
+++ ohgo-backend/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\TransactionController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Auth::routes(['register' => true]);
+
+Route::get('products/{id}/gallery', [ProductController::class, 'gallery'])
+    ->name('products.gallery');
+
+Route::resource('products', ProductController::class);
+
+Route::resource('product-galleries', ProductGalleryController::class);
+
+Route::get('transactions/{id}/set-status', [TransactionController::class, 'setStatus'])
+    ->name('transactions.status');
+
+Route::resource('transactions', TransactionController::class);
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
